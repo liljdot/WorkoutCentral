6ce1b78cbf2e6ff65ec5be16d2d8eedfb93321ae
liljdot <jehuoki@gmail.com>
2025-01-27 20:23:28 +0100

    test

--- server/index.js
+++ server/index.js
@@ -1,41 +1,45 @@
-//get .env variables
-require("dotenv").config()
-const cors = require("cors")
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const workoutRoutes = require("./routes/workouts")
-const userRoutes = require("./routes/users")
+// //get .env variables
+// require("dotenv").config()
+// const cors = require("cors")
+// const express = require("express")
+// const cookieParser = require("cookie-parser")
+// const workoutRoutes = require("./routes/workouts")
+// const userRoutes = require("./routes/users")
 
-// initialize app
-const app = express()
-//set port
-const PORT = process.env.PORT
+// // initialize app
+// const app = express()
+// //set port
+// const PORT = process.env.PORT
 
-//middleware
-    //enable json in requests body
-    app.use(express.json())
+// //middleware
+//     //enable json in requests body
+//     app.use(express.json())
 
-    app.use(cookieParser())
+//     app.use(cookieParser())
 
-    // allow cors. the options allow cookies from/to all origins
-    app.use(cors({
-        origin: (origin, callback) => {
-            // Allow all origins (dynamically set the origin)
-            callback(null, origin || "*");
-          },
-        credentials: true
-    }))
+//     // allow cors. the options allow cookies from/to all origins
+//     app.use(cors({
+//         origin: (origin, callback) => {
+//             // Allow all origins (dynamically set the origin)
+//             callback(null, origin || "*");
+//           },
+//         credentials: true
+//     }))
     
-    app.use((req, res, next) => {
-        console.log(req.method, req.path)
-        next()
-    })
+//     app.use((req, res, next) => {
+//         console.log(req.method, req.path)
+//         next()
+//     })
 
-    //routes
-    app.use('/api/user', userRoutes)
-    app.use('/api/workouts', workoutRoutes)
+//     //routes
+//     app.use('/api/user', userRoutes)
+//     app.use('/api/workouts', workoutRoutes)
 
-//start server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+// //start server
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
+
+const express = require("express")
+const app = express()
+app.listen(3000, () => console.log("hello"))
